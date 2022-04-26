22c36f9559e22eb3c84bf5eed3f14b23ae388173
sunset <20374547@qq.com>
2022-04-26 22:47:33 +0800

    更新

---
+++ 学习笔记/typedef.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+	typedef：为C语言的关键字，作用是为一种数据类型(基本类型或自定义数据类型)定义一个新名字，不能创建新类型。
+*/
+
+typedef int INT; // 给int取别名
+typedef char CHAR; // 给char取别名
+
+typedef struct person
+{
+	char name[128];
+	int age;
+}PEOPLE;// 给结构体取别名
+
+int main(void)
+{
+	INT a = 10;
+	CHAR b = 20;
+	printf("a的大小为：%d个字节。b的大小为：%d个字节。\n", sizeof(a), sizeof(b));
+	PEOPLE people = { "gugu", 18 };
+	printf("%s的年龄是：%d\n", people.name, people.age);
+
+}
